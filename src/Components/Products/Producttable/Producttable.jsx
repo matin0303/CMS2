5344a32d6debf12b57a18b35e78c67c75d19b068
Matin <pirmohammadimatin@gmail.com.com>
2025-02-10 03:09:47 +0330

    added Product Table in Product page

--- src/Components/Products/Producttable/Producttable.jsx
+++ src/Components/Products/Producttable/Producttable.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 export default function Producttable() {
   return (
-    <div>
-      listt
-    </div>
+    <table className='w-3/4 bg-white rounded-2xl mt-10'>
+      <tr className='text-center'>
+        <th>عکس</th>
+        <th>نام</th>
+        <th>قیمت</th>
+        <th>موجودی</th>
+      </tr>
+      <tr className='text-center'>
+        <td className="">
+            <img className='w-35 h-35 object-cover' src="images/airpod.png" alt="product" />
+        </td>
+        <td>
+            ایرپاد
+        </td>
+        <td>
+            1200 تومان
+        </td>
+        <td>
+            492
+        </td>
+        <td>
+            <button className='p-2 m-2 text-white rounded-[0.5rem] bg-blue-custom'>جزئیات</button>
+            <button className='p-2 m-2 text-white rounded-[0.5rem] bg-blue-custom'>حذف</button>
+            <button className='p-2 m-2 text-white rounded-[0.5rem] bg-blue-custom'>ویرایش</button>
+        </td>
+      </tr>
+    </table>
   )
 }
