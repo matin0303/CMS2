45184966c9dd9747ebb29ee7c93c28c920d57aa9
Matin <pirmohammadimatin@gmail.com.com>
2025-03-09 00:53:30 +0330

    added Navlink in sidebar

--- cms2/src/Components/Sidebar/Sidebar.jsx
+++ cms2/src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { AiOutlineHome } from "react-icons/ai";
 import { AiFillProduct } from "react-icons/ai";
 import { FaRegComments } from "react-icons/fa";
@@ -12,37 +12,25 @@ export default function Sidebar() {
   return (
     <div className='fixed bg-blue-custom h-screen  '>
       <h1 className='text-xl text-white p-4 text-right border-b-1 border-solid border-white'>به داشبورد خود خوش آمدید</h1>
-      <ul className='mt-5'> 
-        <li className='relative p-4 activeLink'>
-          <Link to= "/" className=' w-full no-underline text-white flex items-center' >
-            <AiOutlineHome className='text-2xl'/>صفحه اصلی 
-          </Link>
-        </li>
-        <li className='relative p-4'>
-          <Link to="/Products" className=' w-full no-underline text-white flex items-center'>
-            <AiFillProduct/> محصولات
-          </Link>
-        </li>
-        <li className='relative p-4'>
-          <Link to="/Comments" className=' w-full no-underline text-white flex items-center'>
-            <FaRegComments/>  کامنت ها   
-          </Link> 
-        </li>
-        <li className='relative p-4 '>
-          <Link to="/Users" className=' w-full no-underline text-white flex items-center'>
-           <FiUsers/> کاربران
-          </Link>
-        </li>
-        <li className='relative p-4'>
-          <Link to="/Orders" className=' w-full no-underline text-white flex items-center'>
-            <LuListOrdered/>سفارشات
-          </Link>
-        </li>
-        <li className='relative p-4 '>
-          <Link to="Offs" className=' w-full no-underline text-white flex items-center'>
-            <MdOutlineLocalOffer/>تخفیف ها
-          </Link>
-        </li>
+      <ul className='mt-5'>
+        <NavLink to="/" className='relative p-4  w-full no-underline text-white flex items-center'>
+          <AiOutlineHome className='text-2xl' />صفحه اصلی
+        </NavLink>
+        <NavLink to="/Products" className='relative p-4 w-full no-underline text-white flex items-center'>
+          <AiFillProduct /> محصولات
+        </NavLink>
+        <NavLink to="/Comments" className='relative p-4 w-full no-underline text-white flex items-center'>
+          <FaRegComments />  کامنت ها
+        </NavLink>
+        <NavLink to="/Users" className='relative p-4 w-full no-underline text-white flex items-center'>
+          <FiUsers /> کاربران
+        </NavLink>
+        <NavLink to="/Orders" className='relative p-4 w-full no-underline text-white flex items-center'>
+          <LuListOrdered />سفارشات
+        </NavLink>
+        <NavLink to="Offs" className='relative p-4 w-full no-underline text-white flex items-center'>
+          <MdOutlineLocalOffer />تخفیف ها
+        </NavLink>
       </ul>
     </div>
   )
